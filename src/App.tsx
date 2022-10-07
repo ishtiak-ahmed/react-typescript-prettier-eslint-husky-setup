4a8a1ba88c8cc0263397d0b13cd1882e144e0813
Ishtiak Ahmed <iahmed1606@gmail.com>
2022-10-07 20:44:18 +0600

    eslint, prettier, husky config setting added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("test console");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>src/App.tsx</code> and save to reload.
         </p>
+        <h2>Hello world</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
