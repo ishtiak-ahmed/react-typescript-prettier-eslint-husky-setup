0934d8cceb21b7890ad5689fb24899dec2e0f39b
Ishtiak Ahmed <iahmed1606@gmail.com>
2022-10-07 22:17:10 +0600

    react router dom v6 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  console.log("test console");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h2>Hello world</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
